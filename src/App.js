0a488c37d47d2c1ec68184ed7d56a79a8cc693c1
walle <dlauddn95@gmail.com>
2021-11-17 12:34:12 +0900

    :>

--- src/App.js
+++ src/App.js
@@ -1,10 +1,81 @@
-import React from 'react'
-import Hello from './Hello'
-import InputSample from './InputSample';
+import React, { useRef, useState }from 'react'
+import CreateUser from './CreateUser';
+import UserList from './UserList';
 
 function App() {
+
+  const [inputs, setInputs] = useState({
+    username: '',
+    email: '',
+  });
+  const {username, email} = inputs
+
+  const [users, setUsers] = useState([
+    {
+      id: 1,
+      username: 'velopert',
+      email: 'public.velopert@gamil.com',
+      active: true
+    },
+    {
+      id: 2,
+      username: 'tester',
+      email: 'tester@example.com',
+      active: false
+    },
+    {
+      id: 3,
+      username: 'liz',
+      email: 'liz@example.com',
+      active: false
+    },
+  ]);
+
+  const nextId = useRef(4)
+  
+  //입력 input onChange
+  const onChange = (e) => {
+    const { name, value} = e.target;
+    setInputs({
+      ...inputs,
+      [name]: value 
+    })
+  }
+
+  //등록
+  const onCreate = () => {
+    const user = {
+      id: nextId.current,
+      ...inputs
+    };
+
+    setUsers(users.concat(user))
+    //setUsers([...users, user])
+
+    setInputs({
+      username: '',
+      email: ''
+    });
+    nextId.current += 1
+  };
+
+  //삭제
+  const onRemove = id => {
+    setUsers(users.filter(user => user.id !== id))
+  }
+
+  //수정
+  const onToggle = id => {
+    setUsers(users.map(
+      user => user.id === id ? {...user, active: !user.active} : user 
+    ));
+  }
+
   return(
-    <InputSample/>
+    <>
+      <CreateUser username={username} email={email} onChange={onChange} onCreate={onCreate}/>
+      <UserList users={users} onRemove={onRemove} onToggle={onToggle} />
+    </>
   )
 }
 
