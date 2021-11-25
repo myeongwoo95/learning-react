6353cc5326c3b59e47124bbe99df2a8fe85eaac3
walle <dlauddn95@gmail.com>
2021-11-25 13:57:09 +0900

    :>

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useRef, useState, useMemo, useCallback }from 'react'
+import React, { useRef, useMemo, useCallback, useReducer }from 'react'
 import CreateUser from './CreateUser';
 import UserList from './UserList';
 
@@ -7,15 +7,14 @@ function countActiveUsers(users){
   return users.filter(user => user.active).length;
 }
 
-function App() {
+const initialState = {
 
-  const [inputs, setInputs] = useState({
+  inputs: {
     username: '',
     email: '',
-  });
-  const {username, email} = inputs
+  },
 
-  const [users, setUsers] = useState([
+  users: [
     {
       id: 1,
       username: 'velopert',
@@ -34,67 +33,61 @@ function App() {
       email: 'liz@example.com',
       active: false
     },
-  ]);
+  ]
+}
 
+function reducer(state, action){
+  switch(action.type){
+    case 'CHANGE_INPUT':
+      return {
+        ...state,
+        inputs: {
+          ...state.input,
+          [action.name]: action.value
+        }
+      }
+    case 'CREATE_USER':
+      return {
+        //여기서부터
+      }
+    default:
+      throw new Error("Unhandled action")
+  }
+}
+
+function App() {
+  const [state, dispatch] = useReducer(reducer, initialState)
   const nextId = useRef(4)
-  
-  // input onChange
-  const onChange = useCallback(
-    (e) => {
-      const { name, value} = e.target;
+  const { users } = state
+  const { username, email} = state.inputs
 
-      setInputs(inputs => ({
-        ...inputs,
-        [name]: value 
-      }));
-    },
-    []
-  );
+  const onChange = useCallback( e => {
+    const {name, value} = e.target
+    dispatch({
+      type: 'CHANGE_INPUT',
+      name,
+      value
+    })
+    
+  }, [])
 
-  //등록
   const onCreate = useCallback(() => {
-    const user = {
-      id: nextId.current,
-      username,
-      email
-    };
-
-    setUsers( users => users.concat(user))
-    //setUsers([...users, user])
-
-    setInputs({
-      username: '',
-      email: ''
-    });
+    dispatch({
+      type: 'CREATE_USER',
+      user: {
+        id : nextId.current,
+        username,
+        email
+      }
+    })
     nextId.current += 1
-  }, [username, email]);
-
-  //삭제
-  const onRemove = useCallback(
-    id => {
-      // user.id 가 파라미터로 일치하지 않는 원소만 추출해서 새로운 배열을 만듬
-      // = user.id 가 id 인 것을 제거함
-      setUsers( users => users.filter(user => user.id !== id))
-    },
-    []
-  )
-
-  //수정
-  const onToggle = useCallback(id => {
-    setUsers(users =>
-      users.map(user =>
-        user.id === id ? {...user, active: !user.active} : user 
-      )
-    );
-  },[]);
-
-  const count = useMemo(() => countActiveUsers(users), [users]);
+  }, [username, email])
 
   return(
     <>
-      <CreateUser username={username} email={email} onChange={onChange} onCreate={onCreate}  />
-      <UserList users={users} onRemove={onRemove} onToggle={onToggle} />
-      <div>활성 사용자 수 : {count}</div>
+      <CreateUser username = {username} email={email} onChange={onChange} />
+      <UserList users={[]} />
+      <div>활성 사용자 수 : 0</div>
     </>
   )
 }
--- src/Counter.js
+++ src/Counter.js
@@ -1,21 +1,41 @@
-import react, {useState} from 'react';
+import react, {useReducer, useState} from 'react';
+
+function reducer(state, action){
+
+  switch(action.type){
+    case 'INCREMENT':
+      return state + 1
+    case 'DECREMENT':
+      return state - 1
+    default:
+      throw new Error("Unhandled action")
+  }
+}
 
 function Counter(){
   // useState(value) value에는 초기값을 넣어주면 된다.
   //setNumber는 현재 상태를 업데이트하는 함수이다.
-  const [number, setNumber] = useState(0) 
 
-  //위 코드는 아래의 코드를 배열 비구조화 할당을 통해 간결하게 바꾼것이다. 
+  // const [number, setNumber] = useState(0) 
+
+  //위 코드는 아래의 코드를 배열 비구조화 할당을 통해 간결하게 바꾼것 
   //const numberState = numberState(0);
   //const Number = numberState[0]; 첫번째 원소 삽입
   //const setNumber = numberState[1] 두번째 원소 삽입
 
+  // useReducer
+  const [number, dispatch] = useReducer(reducer, 0)
+
   const onIncrease = () => {
-    setNumber(number + 1)
+    dispatch({
+      type: 'INCREMENT'
+    })
   }
 
   const onDecrease = () => {
-    setNumber(number - 1)
+    dispatch({
+      type: 'DECREMENT'
+    })
   }
 
   return (
