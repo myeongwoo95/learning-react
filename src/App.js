7c7b39c2c0c41b8113c3005ec8eb5be9435d7a3c
walle <dlauddn95@gmail.com>
2021-11-26 16:09:55 +0900

    :>

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
-import React, { useRef, useMemo, useCallback, useReducer }from 'react'
+import produce from 'immer';
+import React, { useMemo, useReducer }from 'react'
 import CreateUser from './CreateUser';
 import UserList from './UserList';
-import useInputs from './useInputs';
 
 function countActiveUsers(users){
   console.log("활성 사용자 수를 세는중...")
@@ -32,85 +32,55 @@ const initialState = {
 }
 
 function reducer(state, action) {
-
     switch (action.type) {
         case 'CREATE_USER':
-            return {
-            users: state.users.concat(action.user)
-            };
+            return produce(state, draft => {
+                draft.users.push(action.user)
+            })
+
+            // return {
+            //     users: state.users.concat(action.user)
+            // };
 
         case 'TOGGLE_USER':
-            return {
-            users: state.users.map(user =>
-                user.id === action.id ? { ...user, active: !user.active } : user
-            )
-            };
+            return produce(state, draft => {
+                const user = draft.users.find(user => user.id === action.id);
+                user.active = !user.active;
+            })
+            // return {
+            //     users: state.users.map(user =>
+            //         user.id === action.id ? { ...user, active: !user.active } : user
+            // )
+            //};
 
         case 'REMOVE_USER':
-            return {
-            users: state.users.filter(user => user.id !== action.id)
-            };
+            return produce(state, draft => {
+                const index = draft.users.findIndex(user => user.id === action.id);
+                draft.users.splice(index, 1);
+            })
+            // return {
+            //     users: state.users.filter(user => user.id !== action.id)
+            // };
 
         default:
             return state;
     }
-  }
+}
+
+export const UserDispatch = React.createContext(null);
 
 function App() {
-    const [{username, email}, onChange, reset] = useInputs({
-        username: '',
-        email: ''
-    })
-
     const [state, dispatch] = useReducer(reducer, initialState)
-    const nextId = useRef(4)
-    
     const { users } = state
-
-    const onCreate = useCallback(() => {
-        dispatch({
-            type: 'CREATE_USER',
-            user: {
-                id : nextId.current,
-                username,
-                email
-            }
-        })
-        reset();
-        nextId.current += 1
-    }, [username, email])
-
-    const onToggle = useCallback(id => {
-        dispatch({
-            type: 'TOGGLE_USER',
-            id
-        });
-    }, []);
-    
-    const onRemove = useCallback(id => {
-        dispatch({
-            type: 'REMOVE_USER',
-            id
-        });
-    }, []);
 
     const count = useMemo(() => countActiveUsers(users), [users]);
 
     return(
-        <>
-            <CreateUser
-                username={username}
-                email={email}
-                onChange={onChange}
-                onCreate={onCreate}
-            />
-            <UserList
-                users={users}
-                onToggle={onToggle}
-                onRemove={onRemove}
-            />
+        <UserDispatch.Provider value={dispatch}>
+            <CreateUser />
+            <UserList users={users} />
             <div>활성사용자 수 : {count}</div>
-        </>
+        </UserDispatch.Provider>
     )
 }
 
--- src/UserList.js
+++ src/UserList.js
@@ -1,38 +1,36 @@
-import React from "react"
+import React, { useContext } from "react"
+import { UserDispatch } from './App';
 
-const User = React.memo(function User({user, onRemove, onToggle}){
+const User = React.memo(function User({user}){
+    const dispatch = useContext(UserDispatch);
 
-  // useEffect(() => {
-  //   console.log("user 값이 설정 됨")
-
-  //   return () => {
-  //     console.log('user 값이 바뀌기 전')
-  //   }
-    
-  // }, []);
-
-  return(
-    <div>
-      <b 
-        style={{
-        color: user.active ? 'green' : 'black',
-        cursor: 'pointer' 
-      }} 
-      onClick={() => onToggle(user.id)}>
-        {user.username}
-      </b>
-      <span>({user.email})</span>
-      <button onClick={() => onRemove(user.id)}>삭제</button> 
-    </div> 
-  );
+    return(
+        <div>
+            <b 
+                style={{
+                    color: user.active ? 'green' : 'black',
+                    cursor: 'pointer' 
+                }} 
+                onClick={() => {
+                    dispatch({ type: 'TOGGLE_USER', id: user.id });
+                }}
+            >
+                {user.username}
+            </b>
+            <span>({user.email})</span>
+            <button onClick={() => {
+                dispatch({ type: 'REMOVE_USER', id: user.id });
+            }}>삭제</button> 
+        </div> 
+    );
 });
 
-function UserList({users, onRemove, onToggle}){
+function UserList({users}){
   return (
     <div>
       {
         users.map(
-          user => <User user={user} key={user.id} onRemove={onRemove} onToggle={onToggle}/>
+          user => <User user={user} key={user.id} />
         )
       }
     </div>
